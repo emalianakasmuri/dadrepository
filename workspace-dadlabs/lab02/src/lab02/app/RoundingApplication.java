aaaa43e12a7fe53b4edfd4640ab0943b5e3ff11e
emalianakasmuri <emaliana.kasmuri@gmail.com>
2021-10-08 14:00:38 +0800

    Adding more codes

--- workspace-dadlabs/lab02/src/lab02/app/RoundingApplication.java
+++ workspace-dadlabs/lab02/src/lab02/app/RoundingApplication.java
@@ -13,12 +13,21 @@ public class RoundingApplication {
 
 		System.out.println("Main entry point of RoundingApplication");
 		
+		int divident = 200;
+		int divisor = 357;
 		RoundingManager roundManager = new RoundingManager();
-		double value = roundManager.calculatePercentage(200, 350);
+		double value = roundManager.calculatePercentage(divident, divisor);
 		
-		System.out.println("Value from 350/200 : " + value);
+		System.out.println("Progress from " + divisor + "/" + divident + ": " + value + "%");
 		
-		System.out.println("Round to 2 decimal points: " + roundManager.roundValue(value, 0));
+		
+		int decimalPoints = 2;
+		System.out.println("Round to " + decimalPoints + " decimal points: " 
+				+ roundManager.roundValue(value, decimalPoints) + "%");
+		
+		decimalPoints = 0;
+		System.out.println("Round to " + decimalPoints + " decimal points: " 
+				+ roundManager.roundValue(value, decimalPoints) + "%");
 	}
 
 }
