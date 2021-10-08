44bbe1f620c2cd60774f9f582dcc01c6f77809f7
emalianakasmuri <emaliana.kasmuri@gmail.com>
2021-10-08 11:22:42 +0800

    Creating first class

---
+++ workspace-dadlabs/lab02/src/lab02/app/RoundingApplication.java
@@ -0,0 +1,15 @@
+package lab02.app;
+
+/**
+ * This class demonstrate computing decimal numbers and rounding it to the nearest decimal point.
+ * @author emalianakasmuri
+ *
+ */
+public class RoundingApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Main entry point of RoundingApplication");
+	}
+
+}
